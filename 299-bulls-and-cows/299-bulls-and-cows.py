a78042d5a6fae5c6df30920e8f87b7dce296978f
Mihir Srivastava <63247089+Airodragon@users.noreply.github.com>
2022-06-04 22:42:48 +0530

    Time: 90 ms (10.85%), Space: 14 MB (30.81%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.py
+++ 299-bulls-and-cows/299-bulls-and-cows.py
@@ -1,18 +1,15 @@
 class Solution:
     def getHint(self, secret: str, guess: str) -> str:
         l = len(secret)
-        a = 0
-        b = 0
-        secret = list(secret)
-        guess = list(guess)
+        a,b = 0,0
+        secret,guess = list(secret),list(guess)
         for i in range(l):
             if secret[i]==guess[i]:
-                secret[i]='#'
-                guess[i]='#'
+                secret[i],guess[i]='#','#'
                 a+=1
-        sample = Counter(secret)
+        secret = Counter(secret)
         for i in range(l):
-            if guess[i] in sample and sample[guess[i]]>0 and guess[i]!='#':
-                sample[guess[i]]-=1
+            if guess[i] in secret and secret[guess[i]]>0 and guess[i]!='#':
+                secret[guess[i]]-=1
                 b+=1
         return f"{a}A{b}B"
