adf18f04eeec7613b48a2b7d982f020ddac50c39
Mihir Srivastava <63247089+Airodragon@users.noreply.github.com>
2022-07-13 20:47:00 +0530

    Time: 55 ms (47.23%), Space: 14.2 MB (84.23%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
@@ -1,24 +1,17 @@
+import itertools
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        
-        def solve(k):
-            if len(k)==0:
-                return
-            
-            ans , newk= [], []
-            for node in k:
-                ans.append( node.val )
-                
-                if node.left:
-                    newk.append(node.left)
-                if node.right:
-                    newk.append(node.right)
-            
-            res.append(ans)
-            solve( newk )
-            return
-        
-        res = []
-        if root:
-            solve( [root] )
-        return res
+        if not root:
+            return []
+        ans = []
+        queue = [[root,1]]
+        temp = []
+        while queue:
+            pop_element = queue.pop()
+            temp.append([pop_element[0].val,pop_element[1]])
+            if pop_element[0].left:
+                queue.insert(0,[pop_element[0].left,pop_element[1]+1])
+            if pop_element[0].right:
+                queue.insert(0,[pop_element[0].right,pop_element[1]+1])
+        ans = [[x[0] for x in g] for k, g in itertools.groupby(temp, lambda x: x[1])]
+        return ans
