b9bb45938fbc8a03ef3697b95d6ad083cf40d971
Mihir Srivastava <63247089+Airodragon@users.noreply.github.com>
2022-07-14 17:42:45 +0530

    Time: 109 ms (45.91%), Space: 13.9 MB (83.66%) - LeetHub

--- 1370-increasing-decreasing-string/1370-increasing-decreasing-string.py
+++ 1370-increasing-decreasing-string/1370-increasing-decreasing-string.py
@@ -2,40 +2,17 @@ from collections import Counter
 
 class Solution:
     def sortString(self, s: str) -> str:
-
-        # Count characters and sort them alphabetically
         counts = sorted(Counter(s).items())
-
-        # Initialize the result
         res = []
-
-        # Keep track of if we want to go forward or backward
         reverse = False
-
-        # Iterate until we used all character
         while len(res) != len(s):
-
-            # Iterate through chracters in counts
             for i in range(len(counts)):
-
-                # If reserve==False, i: 0,1,2,3,...
-                # If reserve==True, i: -1,-2,-3,...
                 i = -i - 1 if reverse else i
-
-                # Get a character and its count
                 char, count = counts[i]
-
-                # If its count is 0, skip it
                 if count == 0:
                     continue
-
-                # Else, append the char to the result
                 res.append(char)
-
-                # Update the character count
                 counts[i] = (char, count - 1)
-
-            # Update iterative direction
             reverse = not reverse
 
         return "".join(res)
