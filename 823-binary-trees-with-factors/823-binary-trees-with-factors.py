c57a31e41454fa522ae8cb7e6f35a9b5d84c8df8
Mihir Srivastava <63247089+Airodragon@users.noreply.github.com>
2022-08-10 12:43:52 +0530

    Time: 2730 ms (11.10%), Space: 14.1 MB (57.07%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.py
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.py
@@ -1,19 +1,18 @@
 class Solution:
     def numFactoredBinaryTrees(self, arr: List[int]) -> int:
         arr.sort()
-        dic = []
+        freq = []
+        mod = 10**9 + 7
         for i in arr:
-            dic.append([i,1])
-        for i in range(1,len(dic)):
-            total = dic[i][1]
+            freq.append([i,1])
+        for i in range(1,len(freq)):
+            total = freq[i][1]
             for j in range(0,i):
-                if dic[i][0]%dic[j][0]==0 and dic[i][0]//dic[j][0] in arr:
-                    temp = arr.index(dic[i][0]//dic[j][0])
-                    total +=  dic[j][1]*dic[temp][1]
-            dic[i][1] = total
-        sum1 = 0
-        # print(dic)
-        for i,j in dic:
-            sum1+=j
-        return sum1%((10**9)+7)
-                    
+                if freq[i][0]%freq[j][0]==0 and freq[i][0]//freq[j][0] in arr:
+                    index = arr.index(freq[i][0]//freq[j][0])
+                    total+= (freq[j][1] * freq[index][1])%mod
+            freq[i][1] = total%mod
+        ans = 0
+        for i,j in freq:
+            ans+=j
+        return ans%mod
