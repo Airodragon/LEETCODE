516f6b5890511119270ef29b8f5e8022e5a3857b
Mihir Srivastava <63247089+Airodragon@users.noreply.github.com>
2022-09-08 19:30:40 +0530

    Time: 77 ms (92.95%), Space: 13.9 MB (38.93%) - LeetHub

---
+++ 2032-two-out-of-three/2032-two-out-of-three.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
+        nums1 = list(set(nums1))
+        nums2 = list(set(nums2))
+        nums3 = list(set(nums3))
+        nums2.extend(nums3)
+        nums1.extend(nums2)
+        a = []
+        d = Counter(nums1)
+        for i in d:
+            if d[i]>=2:
+                a.append(i)
+        return a
