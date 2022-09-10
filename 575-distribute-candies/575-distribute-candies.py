f1a21e9442b2350bca56d85d69d2bb9e2f60a9d2
Mihir Srivastava <63247089+Airodragon@users.noreply.github.com>
2022-09-10 21:04:12 +0530

    Time: 2188 ms (5.01%), Space: 16.4 MB (8.62%) - LeetHub

--- 575-distribute-candies/575-distribute-candies.py
+++ 575-distribute-candies/575-distribute-candies.py
@@ -1,7 +1,6 @@
 class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
         n = len(candyType)
-        d = Counter(candyType)
         x = set(candyType)
         if len(x)<n//2:
             return len(x)
